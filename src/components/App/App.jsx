357a40df89b472a9dabc2e84f2bf66f26b58334d
kozhushna <k.kozhushna@gmail.com>
2023-05-02 11:02:10 +0300

    Add pages

---
+++ src/components/App/App.jsx
@@ -0,0 +1,14 @@
+import { Route, Routes } from 'react-router-dom';
+import Home from 'pages/Home';
+import Movies from 'pages/Movies';
+import MovieDetails from 'pages/MovieDetails';
+
+export const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/movies" element={<Movies />} />
+      <Route path="/movies/:movieId" element={<MovieDetails />} />
+    </Routes>
+  );
+};
---
+++ src/pages/Home.js
@@ -0,0 +1,9 @@
+const Home = () => {
+  // useEffect(() => {
+  // HTTP запрос, если нужно
+  // }, [])
+
+  return <div>Домашняя страница</div>;
+};
+
+export default Home;
